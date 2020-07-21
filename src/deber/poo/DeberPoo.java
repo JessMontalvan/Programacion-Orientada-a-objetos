8940aae75031e3a5a12096799dda54b9197d9d9c
Usuario <Usuario@DESKTOP-ROQB35K>
2020-07-20 19:47:03 -0500

    Primer Deber

--- src/deber/poo/DeberPoo.java
+++ src/deber/poo/DeberPoo.java
@@ -6,10 +6,10 @@ public class DeberPoo {
 
     public static void main(String[] args) {
         Scanner suma = new Scanner(System.in);
-        System.out.println("ingrese dos numeros para sumar");
+        System.out.println("Ingrese dos numeros para sumar");
         Suma op = new Suma(suma.nextInt(), suma.nextInt());
-        System.out.println("el resultado de la Suma es: " + op.suma());
-        System.out.println("ingrese los valores para la resta");
+        System.out.println("El resultado de la Suma es: " + op.suma());
+        System.out.println("Ingrese los valores para la resta");
         Scanner resta=new Scanner(System.in);
         Resta res = new Resta(resta.nextInt(),resta.nextInt());
         System.out.println("El resultado de la resta es: "+ res.restar());
